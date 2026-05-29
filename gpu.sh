45b0ec9e11de65c8188f5a3296de464d04c9b132
kingsrule2024 <kingsrule5000@gmail.com>
2026-05-29 09:22:24 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-./miner --pool stratum+tcp://sg1.alphapool.tech:5566 --address prl1pv6yzn7r43jt6yxd7hezg5jez8xh33madelmgcmsdmctqcgm6074s2w54qf --worker $(hostname)
+./miner --pool stratum+tcp://ru1.alphapool.tech:5566 --address prl1pv6yzn7r43jt6yxd7hezg5jez8xh33madelmgcmsdmctqcgm6074s2w54qf --worker $(hostname)
 
 # Echo status
 echo "gpu miner is running now'"
