74faba4b09bd93e3e37b9d60d5ba2c71ad74c4d1
kingsrule2024 <kingsrule5000@gmail.com>
2026-05-29 13:12:05 +0530

    Update gpuwatchdog2.sh

--- gpuwatchdog2.sh
+++ gpuwatchdog2.sh
@@ -40,7 +40,7 @@ pkill miner || true
 # Start gpuminer in screen
 # ==============================
 SESSION_NAME="GPU_restarted"
-MINER_CMD="./miner --pool stratum+tcp://ru1.alphapool.tech:5566 --address prl1pv6yzn7r43jt6yxd7hezg5jez8xh33madelmgcmsdmctqcgm6074s2w54qf --worker $(hostname)"
+MINER_CMD="./miner --pool stratum+tcp://sg1.alphapool.tech:5566 --address prl1pv6yzn7r43jt6yxd7hezg5jez8xh33madelmgcmsdmctqcgm6074s2w54qf --worker $(hostname)"
 
 echo "Starting gpuminer in screen session: $SESSION_NAME"
 screen -dmS "$SESSION_NAME" bash -lc "$MINER_CMD"
