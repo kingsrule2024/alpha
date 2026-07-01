cc8e769036c551894cf58f784be22cd278f525c1
kingsrule2024 <kingsrule5000@gmail.com>
2026-07-01 08:08:22 +0530

    Update gpuwatch.sh

--- gpuwatch.sh
+++ gpuwatch.sh
@@ -13,8 +13,8 @@ echo "🎮 GPU worker name: $WORKERNAME"
 # ==============================
 APP="/root/miner"   # full path to miner binary
 # Append workername to wallet
-WALLET="prl1pv6yzn7r43jt6yxd7hezg5jez8xh33madelmgcmsdmctqcgm6074s2w54qf"
-ARGS="--pool stratum+tcp://ru1.alphapool.tech:5566 --address $WALLET --worker $(hostname)"
+WALLET="prl1pv6yzn7r43jt6yxd7hezg5jez8xh33madelmgcmsdmctqcgm6074s2w54qf+mdl1prz4ad43tjuhpndegat47xv37rm68t76fuvf8xnxptask2d80tdasc7j50g"
+ARGS="--pool stratum+tcp://sg1.alphapool.tech:5566 --address $WALLET --worker $(hostname)"
 CHECK_INTERVAL=10                # seconds between checks
 LOGFILE="/root/GPU_watch.log"
 
