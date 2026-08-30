4a55883a98626b1c19b6ce531e9d20df124466ee
kingsrule2024 <kingsrule5000@gmail.com>
2026-08-30 17:33:39 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-./goldenminer --pubkey=5yuRCLRTqij1SSFfUutoA3PeqAkB5kcXwuEdGdjXTYN1FQMhMotaYGK --name=$(hostname)
+./miner --pubkey=5yuRCLRTqij1SSFfUutoA3PeqAkB5kcXwuEdGdjXTYN1FQMhMotaYGK --name=$(hostname)
 
 # Echo status
 echo "gpu miner is running now'"
